eea168f1c6f5b317a5aa31f9b2207fc135f3e77a
Fredson Silva <92945868+silvafredson@users.noreply.github.com>
2022-02-25 19:45:56 -0300

    Initial Commit

---
+++ Shared/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  Shared
+//
+//  Created by Fredson Silva on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
